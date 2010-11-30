c5763940e1b1701905ea1f2d9733875bc4e76102
arandaa <Alberto@Aranda.edu>
2010-11-29 23:04:06 -0700

    Added Project class to handle projects easier

---
+++ src/com/readytalk/olive/model/Project.java
@@ -0,0 +1,58 @@
+package com.readytalk.olive.model;
+
+import java.io.UnsupportedEncodingException;
+
+public class Project {
+	private String title;
+	private String icon;
+	private User user;
+	public Project(String title, String icon, User user){
+		this.title = title;
+		this.icon = icon;
+		this.user = user;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	@Override
+	public String toString() {
+		return title;
+	}
+	
+	public String sanitize(String input) throws UnsupportedEncodingException {
+		String output = input;
+		
+		// Remove the *really* bad stuff (which cause XSS attacks and SQL
+		// injections).
+		String[] illegalStrings = {"<", ">", "(", ")", "\"", "'", ";"};
+		for (int i = 0; i < illegalStrings.length; ++i) {
+			output = output.replaceAll(illegalStrings[i], "");
+		}
+		
+		// Let encoding convert the rest to safe strings (but not remove).
+		String encoding = "UTF-8";	// This is recommended by the The World Wide Web Consortium Recommendation. See : http://download.oracle.com/javase/1.4.2/docs/api/java/net/URLEncoder.html#encode(java.lang.String, java.lang.String)
+									// Other encodings: http://download.oracle.com/javase/1.4.2/docs/api/java/nio/charset/Charset.html
+		try {
+			output = java.net.URLEncoder.encode(output, encoding);
+		} catch (UnsupportedEncodingException e) {
+			throw new UnsupportedEncodingException("Choose a different encoding than \"" + encoding + "\". " + e.getMessage());
+		}
+		
+		return output;
+	}
+}
