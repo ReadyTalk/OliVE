0490fa5f416d3e2cdb9e976d2edb787cb4d4b1bc
arandaa <Alberto@Aranda.edu>
2011-01-28 11:58:00 -0700

    Cleaning up and refactoring code in OliveLogic and OliveServlet

--- src/com/readytalk/olive/logic/OliveLogic.java
+++ src/com/readytalk/olive/logic/OliveLogic.java
@@ -14,12 +14,34 @@ import com.readytalk.olive.model.Project;
 import com.readytalk.olive.model.User;
 
 public class OliveLogic {
-
+	// CAUTION: Everytime a JDBC connection is created, it MUST be closed after the necessary information is retrieved.
+	
+	
 	// Why some characters are allowed: http://stackoverflow.com/questions/2049502/what-characters-are-allowed-in-email-address
 	private static final String[] illegalStrings = { "!", "&", "*", "(", ")",
 			"-", "=", "{", "}", "[", "]", "\\", "|", ";", "'", "\"", ":", ",",
 			"<", ">", "/", "?", "`" };
+	
+	public static Connection getDBConnection() {
+		try {
+			Context initCtx = new InitialContext();
+			DataSource ds = (DataSource) initCtx
+					.lookup("java:comp/env/jdbc/OliveData");
+			return ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
+	public static void closeConnection(Connection c) {
+		try {
+			c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static Boolean isAuthorized(User user) {
 		try {
 			Connection conn = getDBConnection();
@@ -32,7 +54,6 @@ public class OliveLogic {
 			ResultSet r = st.executeQuery(s);
 			if (r.first()) {
 				closeConnection(conn);
-
 				return true;
 			} else {
 				closeConnection(conn);
@@ -46,6 +67,121 @@ public class OliveLogic {
 		return false; // Error!
 	}
 
+	public static boolean AddAccount(User user) {
+		try {
+			Connection conn = getDBConnection();
+			Statement st = conn.createStatement();
+			String s = "USE OliveData;";
+			st.executeUpdate(s);
+			s = "INSERT INTO Accounts (Username, Password, Name, Email)"
+					+ "VALUES ('" + user.getUsername() + "', Password('"
+					+ user.getPassword() + "') " + ", '" + user.getName()
+					+ "', '" + user.getEmail() + "');";
+			st.executeUpdate(s);
+			s = "SELECT AccountID FROM Accounts WHERE username ='"
+					+ user.getUsername() + "';";
+			ResultSet r = st.executeQuery(s);
+			if (r.first()) {
+				int id = r.getInt("AccountID");
+				user.setAccountID(id);
+			} else {
+				// TODO Add error for rare case that it can't find the data
+			}
+			closeConnection(conn);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static String getName(String username) {
+		try {
+			Connection conn = getDBConnection();
+			Statement st = conn.createStatement();
+			String s = "USE OliveData;";
+			st.executeUpdate(s);
+
+			s = "SELECT Name FROM Accounts WHERE username ='" + username
+					+ "';";
+			ResultSet r = st.executeQuery(s);
+			String name = "";
+			if (r.first()) {
+				name = r.getString("Name");
+			} 
+			closeConnection(conn);
+			return name;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String getEmail(String username) {
+		try {
+			Connection conn = getDBConnection();
+			Statement st = conn.createStatement();
+			String s = "USE OliveData;";
+			st.executeUpdate(s);
+
+			s = "SELECT Email FROM Accounts WHERE username ='" + username
+					+ "';";
+			ResultSet r = st.executeQuery(s);
+			String email = "";
+			if (r.first()) {
+				email = r.getString("Email");
+			}
+			closeConnection(conn);
+			return email;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static Boolean editAccount(User user) {
+		try {
+			Connection conn = getDBConnection();
+			Statement st = conn.createStatement();
+			String s = "USE OliveData;";
+			st.executeUpdate(s);
+			s = "UPDATE Accounts SET Name = '" + user.getName()
+					+ "' WHERE Username " + "= '" + user.getUsername() + "'";
+			st.executeUpdate(s);
+
+			s = "UPDATE Accounts SET Password = Password('"
+					+ user.getPassword() + "') WHERE Username " + "= '"
+					+ user.getUsername() + "'";
+			st.executeUpdate(s);
+
+			s = "UPDATE Accounts SET Email = '" + user.getEmail()
+					+ "' WHERE Username " + "= '" + user.getUsername() + "'";
+			st.executeUpdate(s);
+			closeConnection(conn);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static void deleteAccount(User user) {
+		try {
+			Connection conn = getDBConnection();
+			Statement st = conn.createStatement();
+			String s = "USE OliveData;";
+			st.executeUpdate(s);
+			s = "DELETE FROM Accounts WHERE" + "username = '"
+					+ user.getUsername() + "';"; // Need to add error checking
+			st.executeUpdate(s);
+			closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static String populateProjects(User user) {
 
 		String projects = "";
@@ -55,6 +191,7 @@ public class OliveLogic {
 			String s = "USE OliveData;";
 			st.executeUpdate(s);
 			if (user == null) {
+				closeConnection(conn);
 				return projects;
 			}
 			if (user.getAccountID() == -1) {
@@ -96,6 +233,7 @@ public class OliveLogic {
 							+ "\n" + "</div>" + "\n";
 				} while (r.next());
 			}
+			closeConnection(conn);
 			return projects;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -103,26 +241,6 @@ public class OliveLogic {
 		return projects;
 		// TODO change db to have unique usernames for accounts and names for both projects and videos in one project
 
-	}
-
-	public static Connection getDBConnection() {
-		try {
-			Context initCtx = new InitialContext();
-			DataSource ds = (DataSource) initCtx
-					.lookup("java:comp/env/jdbc/OliveData");
-			return ds.getConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static void closeConnection(Connection c) {
-		try {
-			c.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 	public static boolean projectExists(String projectTitle, String username) {
@@ -139,6 +257,7 @@ public class OliveLogic {
 			if (r.first()) {
 				accountID = r.getInt("AccountID");
 			} else {
+				closeConnection(conn);
 				return false; // Username does not exist.
 			}
 
@@ -146,8 +265,10 @@ public class OliveLogic {
 					+ "' AND AccountID = '" + accountID + "';";
 			r = st.executeQuery(s);
 			if (r.first()) {
+				closeConnection(conn);
 				return true;
 			} else {
+				closeConnection(conn);
 				return false;
 			}
 
@@ -157,33 +278,7 @@ public class OliveLogic {
 		return false;
 	}
 
-	public static boolean AddAccount(User user) {
-		try {
-			Connection conn = getDBConnection();
-			Statement st = conn.createStatement();
-			String s = "USE OliveData;";
-			st.executeUpdate(s);
-			s = "INSERT INTO Accounts (Username, Password, Name, Email)"
-					+ "VALUES ('" + user.getUsername() + "', Password('"
-					+ user.getPassword() + "') " + ", '" + user.getName()
-					+ "', '" + user.getEmail() + "');";
-			st.executeUpdate(s);
-			s = "SELECT AccountID FROM Accounts WHERE username ='"
-					+ user.getUsername() + "';";
-			ResultSet r = st.executeQuery(s);
-			if (r.first()) {
-				int id = r.getInt("AccountID");
-				user.setAccountID(id);
-			} else {
-				// TODO Add error for rare case that it can't find the data
-			}
-			closeConnection(conn);
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+	
 
 	public static void AddProject(Project p, User u) {
 		try {
@@ -211,6 +306,21 @@ public class OliveLogic {
 		}
 	}
 
+	public static void deleteProject(String name, int AccountID) {
+		try {
+			Connection conn = getDBConnection();
+			Statement st = conn.createStatement();
+			String s = "USE OliveData;";
+			st.executeUpdate(s);
+			s = "DELETE FROM Projects WHERE" + "Name = '" + name
+					+ "' AND AccountID = '" + AccountID + "';"; // Need to add error checking
+			st.executeUpdate(s);
+			closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void AddVideo(String name, String URL, int ProjectID,
 			String icon) {
 		try {
@@ -221,36 +331,6 @@ public class OliveLogic {
 			s = "INSERT INTO Videos (Name, URL, ProjectID, Icon)" + "VALUES ('"
 					+ name + "', '" + URL + "', '" + ProjectID + "' , '" + icon
 					+ "');";
-			st.executeUpdate(s);
-			closeConnection(conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void deleteAccount(User user) {
-		try {
-			Connection conn = getDBConnection();
-			Statement st = conn.createStatement();
-			String s = "USE OliveData;";
-			st.executeUpdate(s);
-			s = "DELETE FROM Accounts WHERE" + "username = '"
-					+ user.getUsername() + "';"; // Need to add error checking
-			st.executeUpdate(s);
-			closeConnection(conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void deleteProject(String name, int AccountID) {
-		try {
-			Connection conn = getDBConnection();
-			Statement st = conn.createStatement();
-			String s = "USE OliveData;";
-			st.executeUpdate(s);
-			s = "DELETE FROM Projects WHERE" + "Name = '" + name
-					+ "' AND AccountID = '" + AccountID + "';"; // Need to add error checking
 			st.executeUpdate(s);
 			closeConnection(conn);
 		} catch (Exception e) {
@@ -272,31 +352,7 @@ public class OliveLogic {
 		}
 	}
 
-	public static Boolean editAccount(User user) {
-		try {
-			Connection conn = getDBConnection();
-			Statement st = conn.createStatement();
-			String s = "USE OliveData;";
-			st.executeUpdate(s);
-			s = "UPDATE Accounts SET Name = '" + user.getName()
-					+ "' WHERE Username " + "= '" + user.getUsername() + "'";
-			st.executeUpdate(s);
-
-			s = "UPDATE Accounts SET Password = Password('"
-					+ user.getPassword() + "') WHERE Username " + "= '"
-					+ user.getUsername() + "'";
-			st.executeUpdate(s);
-
-			s = "UPDATE Accounts SET Email = '" + user.getEmail()
-					+ "' WHERE Username " + "= '" + user.getUsername() + "'";
-			st.executeUpdate(s);
-			closeConnection(conn);
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+	
 
 	// TODO Create multiple methods for different kinds of input
 	public static boolean isSafe(String input)
@@ -330,47 +386,5 @@ public class OliveLogic {
 		return output;
 	}
 
-	public static String getName(String username) {
-		try {
-			Connection conn = getDBConnection();
-			Statement st = conn.createStatement();
-			String s = "USE OliveData;";
-			st.executeUpdate(s);
-
-			s = "SELECT Name FROM Accounts WHERE username ='" + username
-					+ "';";
-			ResultSet r = st.executeQuery(s);
-			String name = "";
-			if (r.first()) {
-				name = r.getString("Name");
-			} 
-			return name;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static String getEmail(String username) {
-		try {
-			Connection conn = getDBConnection();
-			Statement st = conn.createStatement();
-			String s = "USE OliveData;";
-			st.executeUpdate(s);
-
-			s = "SELECT Email FROM Accounts WHERE username ='" + username
-					+ "';";
-			ResultSet r = st.executeQuery(s);
-			String email = "";
-			if (r.first()) {
-				email = r.getString("Email");
-			} 
-			return email;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	
 }
--- src/com/readytalk/olive/servlet/OliveServlet.java
+++ src/com/readytalk/olive/servlet/OliveServlet.java
@@ -1,6 +1,7 @@
 package com.readytalk.olive.servlet;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -15,124 +16,131 @@ import com.readytalk.olive.model.User;
 
 public class OliveServlet extends HttpServlet {
 
-	private static Logger log = Logger.getLogger(OliveServlet.class.getName());
+	
 
 	// Don't store anything as a member variable in the Servlet.
 	// private Object dontDoThis;
 
+	//Private static variables are ok
+	private static Logger log = Logger.getLogger(OliveServlet.class.getName());
+		
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		/*
-		 * The log will have .severe (very bad) .warning (uh oh) and .info
-		 * 
-		 * You probably want to always use the .info method to log.
-		 */
 		String id = request.getParameter("FormName");
-		log
-				.info("This is a servlet responding to an Http POST request from form: "
-						+ id);
-
-		Boolean isAuthorized;
-
+		log.info("This is a servlet responding to "
+				+ "an Http POST request from form: " + id);
 		if (id.equals("LoginUser")) {
-
-			String username = request.getParameter("username");
-			String password = request.getParameter("password");
-			if (OliveLogic.isSafe(username) && OliveLogic.isSafe(password)) {
-				session.setAttribute("isSafe", true);
-				User user = new User(username, password, OliveLogic.getEmail(username), OliveLogic.getName(username));
-				isAuthorized = OliveLogic.isAuthorized(user);
-				session.setAttribute("isAuthorized", isAuthorized); // Do not redisplay user name (XSS vulnerability).
-				if (isAuthorized) { // Take the user to the projects page.
-					session.setAttribute("username", user.getUsername());
-					session.setAttribute("password", user.getPassword());
-					session.setAttribute("email", user.getEmail());
-					session.setAttribute("name", user.getName());
-					session.removeAttribute("isSafe"); // Cleared so as to not interfere with any other form
-					response.sendRedirect("projects.jsp");
-				} else { // Keep the user on the same page.
-					response.sendRedirect("index.jsp");
-				}
-			} else {
-				session.setAttribute("isSafe", false);
-				session.setAttribute("isAuthorized", false);
-				response.sendRedirect("index.jsp");
-			}
-
+			loginHandler(request, response, session);
 		} else if (id.equals("EditUser")) {
-			String username = (String) session.getAttribute("username");
-			String newName = request.getParameter("new-name");
-			String newEmail = request.getParameter("new-email");
-			String newPassword = request.getParameter("new-password");
-			String confirmNewPassword = request
-					.getParameter("confirm-new-password");
-			if (OliveLogic.isSafe(newName) && OliveLogic.isSafe(newEmail)
-					&& OliveLogic.isSafe(newPassword)
-					&& OliveLogic.isSafe(confirmNewPassword)) {
-				if (newPassword.equals(confirmNewPassword)) {
-					User updateUser = new User(username, newPassword, newEmail,
-							newName);
-					Boolean editSuccessfully = OliveLogic
-							.editAccount(updateUser);
-					session.setAttribute("editSuccessfully", editSuccessfully);
-					session.setAttribute("passwordsMatch", true);
-					session.setAttribute("password", newPassword);
-					session.setAttribute("email", newEmail);
-					session.setAttribute("name", newName);
-				} else {
-					session.setAttribute("editSuccessfully", false);
-					session.setAttribute("passwordsMatch", false);
-				}
-			} else {
-				session.setAttribute("editSuccessfully", false);
-			}
-			response.sendRedirect("account.jsp");
-
+			editUserHandler(request, response, session);
 		} else if (id.equals("AddUser")) {
-			// The jQuery regex should catch malicious input, but sanitize just to be safe.
-			String username = OliveLogic.sanitize(request.getParameter("name"));
-			String password = OliveLogic.sanitize(request
-					.getParameter("password"));
-			String email = OliveLogic.sanitize(request.getParameter("email"));
-			User newUser = new User(username, password, email, username);
-			Boolean addSuccessfully = OliveLogic.AddAccount(newUser);
-			if (addSuccessfully) {
-				session.setAttribute("isAuthorized", true);
-				session.setAttribute("username", username);
-				session.setAttribute("password", password);
-				session.setAttribute("email",email);
-				response.sendRedirect("projects.jsp");
-			} else {
-				response.sendRedirect("index.jsp");
-				// TODO Add error message here
-			}
-		}
-
-		else if (id.equals("AddProject")) {
-			/*
-			 * PrintWriter out = response.getWriter();
-			 * response.setContentType("text/plain");
-			 * out.println("Project Created. Please close this window and refresh the projects page.");
-			 */
-			String projectName = request.getParameter("ProjectName");
-			if (OliveLogic.isSafe(projectName)) {
-				session.setAttribute("isSafe", true);
-				// Adding the project information to the database
-				User user = new User((String) session.getAttribute("username"),
-						(String) session.getAttribute("password"));
-				Project project = new Project(projectName, user);
-				OliveLogic.AddProject(project, user);
-			} else {
-				session.setAttribute("isSafe", false);
-			}
-			response.sendRedirect("newProjectForm.jsp");
-
+			addUserHandler(request, response, session);
+		} else if (id.equals("AddProject")) {
+			addProjectHandler(request, response, session);
 		}
 
 	}
 
+	private void loginHandler(HttpServletRequest request,
+			HttpServletResponse response, HttpSession session)
+			throws UnsupportedEncodingException, IOException {
+		Boolean isAuthorized;
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (OliveLogic.isSafe(username) && OliveLogic.isSafe(password)) {
+			session.setAttribute("isSafe", true);
+			User user = new User(username, password, OliveLogic
+					.getEmail(username), OliveLogic.getName(username));
+			isAuthorized = OliveLogic.isAuthorized(user);
+			session.setAttribute("isAuthorized", isAuthorized); // Do not redisplay user name (XSS vulnerability).
+			if (isAuthorized) { // Take the user to the projects page.
+				session.setAttribute("username", user.getUsername());
+				session.setAttribute("password", user.getPassword());
+				session.setAttribute("email", user.getEmail());
+				session.setAttribute("name", user.getName());
+				session.removeAttribute("isSafe"); // Cleared so as to not interfere with any other form
+				response.sendRedirect("projects.jsp");
+			} else { // Keep the user on the same page.
+				response.sendRedirect("index.jsp");
+			}
+		} else {
+			session.setAttribute("isSafe", false);
+			session.setAttribute("isAuthorized", false);
+			response.sendRedirect("index.jsp");
+		}
+	}
+
+	private void editUserHandler(HttpServletRequest request,
+			HttpServletResponse response, HttpSession session)
+			throws UnsupportedEncodingException, IOException {
+		String username = (String) session.getAttribute("username");
+		String newName = request.getParameter("new-name");
+		String newEmail = request.getParameter("new-email");
+		String newPassword = request.getParameter("new-password");
+		String confirmNewPassword = request
+				.getParameter("confirm-new-password");
+		if (OliveLogic.isSafe(newName) && OliveLogic.isSafe(newEmail)
+				&& OliveLogic.isSafe(newPassword)
+				&& OliveLogic.isSafe(confirmNewPassword)) {
+			if (newPassword.equals(confirmNewPassword)) {
+				User updateUser = new User(username, newPassword, newEmail,
+						newName);
+				Boolean editSuccessfully = OliveLogic.editAccount(updateUser);
+				session.setAttribute("editSuccessfully", editSuccessfully);
+				session.setAttribute("passwordsMatch", true);
+				session.setAttribute("password", newPassword);
+				session.setAttribute("email", newEmail);
+				session.setAttribute("name", newName);
+			} else {
+				session.setAttribute("editSuccessfully", false);
+				session.setAttribute("passwordsMatch", false);
+			}
+		} else {
+			session.setAttribute("editSuccessfully", false);
+		}
+		response.sendRedirect("account.jsp");
+	}
+	
+	private void addUserHandler(HttpServletRequest request,
+			HttpServletResponse response, HttpSession session)
+			throws IOException {
+		// The jQuery regex should catch malicious input, but sanitize just to be safe.
+		String username = OliveLogic.sanitize(request.getParameter("name"));
+		String password = OliveLogic.sanitize(request.getParameter("password"));
+		String email = OliveLogic.sanitize(request.getParameter("email"));
+		User newUser = new User(username, password, email, username);
+		Boolean addSuccessfully = OliveLogic.AddAccount(newUser);
+		if (addSuccessfully) {
+			session.setAttribute("isAuthorized", true);
+			session.setAttribute("username", username);
+			session.setAttribute("password", password);
+			session.setAttribute("email", email);
+			response.sendRedirect("projects.jsp");
+		} else {
+			response.sendRedirect("index.jsp");
+			// TODO Add error message here
+		}
+	}
+
+	private void addProjectHandler(HttpServletRequest request,
+			HttpServletResponse response, HttpSession session)
+			throws UnsupportedEncodingException, IOException {
+		String projectName = request.getParameter("ProjectName");
+		if (OliveLogic.isSafe(projectName)) {
+			session.setAttribute("isSafe", true);
+			// Adding the project information to the database
+			User user = new User((String) session.getAttribute("username"),
+					(String) session.getAttribute("password"));
+			Project project = new Project(projectName, user);
+			OliveLogic.AddProject(project, user);
+		} else {
+			session.setAttribute("isSafe", false);
+		}
+		response.sendRedirect("newProjectForm.jsp");
+	}
+	
 	// http://www.apl.jhu.edu/~hall/java/Servlet-Tutorial/Servlet-Tutorial-Form-Data.html
 	@Override
 	protected void doGet(HttpServletRequest request,
