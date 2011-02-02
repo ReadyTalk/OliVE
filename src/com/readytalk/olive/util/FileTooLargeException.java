d445d773c6422f256715133fab83dbb53c447ccb
Terry Smith <terry@smith>
2011-02-01 21:15:21 -0700

    Added JetS3t 3rd-party properties files (and modified runtime classpath to accommodate them). Added size-checking for video uploading (limit of 30 MB).

---
+++ src/com/readytalk/olive/util/FileTooLargeException.java
@@ -0,0 +1,27 @@
+package com.readytalk.olive.util;
+
+public class FileTooLargeException extends Exception {
+	
+	// Created using Eclipse's "Add generated serial version ID" refactoring.
+	private static final long serialVersionUID = 1699131254533294330L;
+	
+	public FileTooLargeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileTooLargeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileTooLargeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileTooLargeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
