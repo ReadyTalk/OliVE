10da027b6997b32bbeafa703968b199512f1401e
arandaa <Alberto@Aranda.edu>
2011-03-13 18:33:09 -0600

    Sort of stable code-ish

--- WebContent/scripts/editor.js
+++ WebContent/scripts/editor.js
@@ -11,7 +11,7 @@ jQuery(function($) {
 	attachVideoClickHandlers();
 	attachVideoRenameHandlers();
 	enableDragAndDrop();
-	attachPublishButtonHandler();
+	//attachPublishButtonHandler();
 	getVideoInformation();
 });
 
